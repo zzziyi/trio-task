259564852edaeb0006491880b8027f4aaf2a9338
zzziyi <wenziyichen@gmail.com>
2023-11-02 18:19:54 +0000

    modification

--- flask-app/app.py
+++ flask-app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 db = SQLAlchemy(app)
 
 # Replace [PASSWORD] with the root password for your mysql container
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:[PASSWORD]@mysql:3306/flask-db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:rootpw@db:3306/flask-db'
 
 class Users(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
